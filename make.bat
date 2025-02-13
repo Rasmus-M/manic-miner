6cce17dd9d2e03f050eb511622cca615f9bba541
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-02-13 17:08:32 +0100

    Demo

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 xas99.py src/main-memory.a99 -i -q -R -L manicminer.lst -E symbols.txt -o bin/main
 xas99.py src/rom-banks.a99 -B -q -R -o bin/banks.bin
 
-java -jar tools/ea5tocart.jar bin\main "MANIC MINER"
+java -jar tools/ea5tocart.jar bin\main "MANIC MINER DEMO"
 
 copy /b bin\main8.bin ^
     + bin\banks.bin ^
