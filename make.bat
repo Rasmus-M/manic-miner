da4c4525bb8e848b1fb2727f6ce26492e3927e3f
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-02-17 11:24:28 +0100

    Hardware sprites WIP

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 xas99.py src/main-memory.a99 -i -q -R -L manicminer.lst -E symbols.txt -o bin/main
 xas99.py src/rom-banks.a99 -B -q -R -o bin/banks.bin
 
-java -jar tools/ea5tocart.jar bin\main "MANIC MINER DEMO"
+java -jar tools/ea5tocart.jar bin\main "MANIC MINER" > make.log
 
 copy /b bin\main8.bin ^
     + bin\banks.bin ^
